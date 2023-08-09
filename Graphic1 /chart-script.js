b747b60262276fe4061f1d2731f852fb2688455a
yuntkim <y@Yuns-MacBook-Pro.local>
2023-08-08 20:17:00 -0700

    uploading final 2 graphics

--- Graphic1 /chart-script.js
+++ Graphic1 /chart-script.js
@@ -1,79 +1,69 @@
 // chart-script.js
-
-// Sample data for exoplanet discovery methods
-var discoveryMethodsData = [
-  { method: 'Radial Velocity', count: 1200 },
-  { method: 'Transit', count: 980 },
-  { method: 'Microlensing', count: 450 },
-  { method: 'Direct Imaging', count: 300 },
-  { method: 'Astrometry', count: 200 },
-  { method: 'Pulsar Timing', count: 150 },
-  { method: 'Orbital Brightness Modulation', count: 130 },
-  { method: 'Transit Timing Variations', count: 100 },
-  { method: 'Eclipse Timing Variations', count: 80 },
-  { method: 'Pulsation Timing Variations', count: 70 }
-];
-
-// Extract method names and counts for the chart
-var methodNames = discoveryMethodsData.map(data => data.method);
-var methodCounts = discoveryMethodsData.map(data => data.count);
-
-// Get the canvas element for the bar chart
-var barCtx = document.getElementById('barChart').getContext('2d');
-
-// Create the bar chart
-var barChart = new Chart(barCtx, {
-  type: 'bar',
-  data: {
-    labels: methodNames,
-    datasets: [{
-      data: methodCounts,
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.8)',  // Red
-        'rgba(54, 162, 235, 0.8)', // Blue
-        'rgba(255, 205, 86, 0.8)', // Yellow
-        'rgba(75, 192, 192, 0.8)', // Light blue
-        'rgba(153, 102, 255, 0.8)', // Purple
-        'rgba(255, 159, 64, 0.8)', // Orange
-        'rgba(201, 203, 207, 0.8)', // Gray
-        'rgba(99, 255, 132, 0.8)', // Green
-        'rgba(235, 162, 54, 0.8)', // Orange
-        'rgba(86, 205, 255, 0.8)'  // Light blue
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 205, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-        'rgba(201, 203, 207, 1)',
-        'rgba(99, 255, 132, 1)',
-        'rgba(235, 162, 54, 1)',
-        'rgba(86, 205, 255, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    animation: {
-      duration: 2000, // Animation duration in milliseconds
-      easing: 'easeInOutExpo' // Easing function for animation
-    },
-    scales: {
-      y: {
-        beginAtZero: true,
-        title: {
-          display: true,
-          text: 'Number of Exoplanets Discovered'
+// Fetch the JSON data
+fetch('exoplanet_data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Prepare data for the chart
+    var methodsData = {};
+    var uniqueMethods = [...new Set(data.map(item => item.discoverymethod))];
+    uniqueMethods.forEach(method => {
+      methodsData[method] = {};
+      data.filter(item => item.discoverymethod === method).forEach(item => {
+        if (!methodsData[method][item.disc_year]) {
+          methodsData[method][item.disc_year] = 0;
         }
+        methodsData[method][item.disc_year] += 1;
+      });
+    });
+    var years = Object.keys(data.reduce((acc, item) => {
+      acc[item.disc_year] = true;
+      return acc;
+    }, {}));
+    var datasets = uniqueMethods.map((method, index) => {
+      var methodCounts = years.map(year => methodsData[method][year] || 0);
+      return {
+        label: method,
+        data: methodCounts,
+        backgroundColor: 'rgba(' + (index * 50) + ', 120, 200, 0.8)', // Generate colors
+        borderWidth: 1
+      };
+    });
+    // Get the canvas element for the bar chart
+    var barCtx = document.getElementById('barChart').getContext('2d');
+    // Create the bar chart
+    var barChart = new Chart(barCtx, {
+      type: 'bar',
+      data: {
+        labels: years,
+        datasets: datasets
       },
-      x: {
-        title: {
-          display: true,
-          text: 'Discovery Methods'
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            stacked: true,
+            title: {
+              display: true,
+              text: 'Number of Exoplanets Discovered'
+            }
+          },
+          x: {
+            stacked: true,
+            title: {
+              display: true,
+              text: 'Years'
+            }
+          }
+        },
+        plugins: {
+          legend: {
+            display: true,
+            position: 'bottom' // Position of the legend
+          }
         }
       }
-    }
-  }
-});
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching or processing data:', error);
+  });
--- Graphic3/chart-script.js
+++ Graphic3/chart-script.js
@@ -1,103 +1,85 @@
-// chart-script.js
-
-// Sample data (replace this with actual data fetched from the backend)
-var exoplanetData = generateRandomExoplanetData(4000, 35);
-
-// Generate random exoplanet data within specified time and distance ranges
-function generateRandomExoplanetData(count, timeRange) {
-  var data = [];
-
-  for (var i = 0; i < count; i++) {
-    data.push({
-      year: Math.floor(Math.random() * timeRange) + 1989, // Adjusted year range
-      distance: Math.floor(Math.random() * 10), // Generate whole numbers for distances
-    });
-  }
-
-  return data.sort((a, b) => a.year - b.year); // Sort by oldest to most recent
-}
-
-// Get color based on distance
-function getColorByDistance(distance) {
-  var distanceCategories = [
-    { maxDistance: 2, color: 'rgba(255, 99, 132, 0.8)' }, // Red
-    { maxDistance: 4, color: 'rgba(255, 159, 64, 0.8)' }, // Orange
-    { maxDistance: 6, color: 'rgba(255, 205, 86, 0.8)' }, // Yellow
-    { maxDistance: 8, color: 'rgba(75, 192, 192, 0.8)' }, // Light blue
-    { maxDistance: 10, color: 'rgba(54, 162, 235, 0.8)' } // Blue
-  ];
-
-  for (var i = 0; i < distanceCategories.length; i++) {
-    if (distance <= distanceCategories[i].maxDistance) {
-      return distanceCategories[i].color;
-    }
-  }
-
-  return 'rgba(201, 203, 207, 0.8)'; // Gray for unknown distances
-}
-
-// Calculate bubble size based on distance
-function getBubbleSize(distance) {
-  return 10 - distance;
-}
-
-// Get the canvas element for the scatter plot chart
-var scatterCtx = document.getElementById('scatterChart').getContext('2d');
-
-// Create the scatter plot chart
-var scatterChart = new Chart(scatterCtx, {
-  type: 'scatter',
-  data: {
-    datasets: [{
-      label: 'Exoplanet Distances',
-      data: [],
-      pointBackgroundColor: [],
-      pointRadius: [],
-      pointHoverRadius: 8
-    }]
-  },
-  options: {
-    animation: {
-      duration: 1000, // Animation duration in milliseconds
-    },
-    scales: {
-      x: {
-        type: 'linear',
-        position: 'bottom',
-        title: {
-          display: true,
-          text: 'Year'
-        }
+// Define the color gradient
+const colorGradient = [
+  'rgba(255, 0, 255, 0.2)', // Magenta
+  'rgba(255, 0, 0, 0.8)' // Red
+];
+// Fetch the data
+fetch('exoplanet_data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Calculate the count of discoveries per year
+    const countByYear = data.reduce((count, exoplanet) => {
+      const year = exoplanet.disc_year;
+      count[year] = (count[year] || 0) + 1;
+      return count;
+    }, {});
+    // Extract the required data for plotting
+    const chartData = data.map(exoplanet => ({
+      x: exoplanet.disc_year,
+      y: exoplanet.pl_orbsmax,
+      count: countByYear[exoplanet.disc_year]
+    }));
+    // Get the canvas element for the scatter plot chart
+    const scatterCtx = document.getElementById('scatterChart').getContext('2d');
+    // Create the scatter plot chart
+    const scatterChart = new Chart(scatterCtx, {
+      type: 'scatter',
+      data: {
+        datasets: [{
+          label: 'Exoplanet Distances',
+          data: chartData,
+          pointBackgroundColor: chartData.map(exoplanet => getGradientColor(exoplanet.count)),
+          pointRadius: 5,
+          pointHoverRadius: 8
+        }]
       },
-      y: {
-        beginAtZero: true,
-        title: {
-          display: true,
-          text: 'Distance from Earth (light-years)'
+      options: {
+        scales: {
+          x: {
+            type: 'linear',
+            position: 'bottom',
+            title: {
+              display: true,
+              text: 'Year Discovered'
+            },
+            ticks: {
+              callback: function (value) {
+                return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '');
+              }
+            }
+          },
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Distance from Star (AU)'
+            }
+          }
+        },
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label: function (context) {
+                let label = '';
+                if (context.parsed.x !== null) {
+                  label = 'Year: ' + context.parsed.x + '\n';
+                }
+                if (context.parsed.y !== null) {
+                  label += 'Distance: ' + context.parsed.y + ' AU';
+                }
+                return label;
+              }
+            }
+          }
         }
       }
+    });
+    // Function to get the gradient color based on the count
+    function getGradientColor(count) {
+      const gradientIndex = Math.min(count - 1, colorGradient.length - 1);
+      return colorGradient[gradientIndex];
     }
-  }
-});
-
-// Function to start the scatter plot animation
-function startScatterPlotAnimation() {
-  var dataIndex = 0;
-
-  var animation = setInterval(function () {
-    var exoplanet = exoplanetData[dataIndex];
-
-    if (exoplanet) {
-      scatterChart.data.datasets[0].data.push({ x: exoplanet.year, y: exoplanet.distance });
-      scatterChart.data.datasets[0].pointBackgroundColor.push(getColorByDistance(exoplanet.distance));
-      scatterChart.data.datasets[0].pointRadius.push(getBubbleSize(exoplanet.distance));
-      scatterChart.update();
-      dataIndex++;
-    } else {
-      clearInterval(animation);
-    }
-  }, 100); // Interval between adding data points (milliseconds)
-}
-
-// Start the scatter plot animation
-startScatterPlotAnimation();
+  })
+  .catch(error => {
+    console.error('Error fetching exoplanet data:', error);
+  });
